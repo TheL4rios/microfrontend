f8767ed4a69023cff5c0e33b3cca1500b72cbdad
Arturo Larios <pinsasmetal@gmail.com>
2024-03-21 21:48:18 -0600

    save

--- micros/mf-header/app2/src/app/app-routing.module.ts
+++ micros/mf-header/app2/src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { TestComponent } from "./test/test.component";
 
 const routes: Routes = [
   {
-    path: '**',
-    component: AppComponent
-  }
+    path: "x/2",
+    component: TestComponent,
+  },
+  {
+    path: "**",
+    component: AppComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- micros/mf-header/app2/src/app/app.component.ts
+++ micros/mf-header/app2/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app2';
+  title = "app2";
+
+  user: any = null;
+
+  constructor() {}
+
+  setUser() {
+    this.user = this.user ? null : { name: "Larios" };
+    localStorage.setItem("userData", JSON.stringify(this.user));
+    window.dispatchEvent(new Event("storage"));
+  }
 }
